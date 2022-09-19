3a38c6069b4a21ff0072e9e230ce5a5aa41c1afc
tateshi <yarumaya0822@gmail.com>
2022-09-19 11:34:57 +0900

    feat: componentの作成

--- src/components/InputTask/InputTask.tsx
+++ src/components/InputTask/InputTask.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import  './InputTask.css'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
+import { inputTitleState } from '../../states/inputTitleState'
 
 const InputTask = () => {
+  const inputTitle = useRecoilValue(inputTitleState)
+  const setInputTitle = useSetRecoilState(inputTitleState)
+
+  const handleClick =() => {
+    console.log(inputTitle)
+  }
+
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputTitle(e.target.value)
+  },[inputTitle])
+
   return (
     <div className='inputField'>
-      <input type='text' className='inputTitle'/>
-      <button type='button' className='addButton'>追加</button>
+      <input type='text' className='inputTitle' onChange={onChange}/>
+      <button type='button' className='addButton' onClick={handleClick}>追加</button>
     </div>
   )
 }
